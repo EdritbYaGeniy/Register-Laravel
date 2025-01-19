a3713bf228ef6622eaed9c7bc529759da87aaff3
EdritbYaGeniy <sergeygugiev67843@mail.ru>
2025-01-19 09:28:04 +0300

    Unreal Commit

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Models\RegistrationModel;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+    ]);
+
+        $token = $user->createToken('RegistrationToken')->accessToken;
+
+        return response()->json(['user' => $user, 'token' => $token], 201);
+    }
+}
